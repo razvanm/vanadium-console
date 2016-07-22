cdf99a9608209d26898091db2bd1791a6a11e01c
Razvan Musaloiu-E <razvanm@google.com>
2016-07-22 08:54:46 -0700

    Don't open a window if we don't get a token

--- app/background.js
+++ app/background.js
@@ -2,9 +2,12 @@ function onLaunched(launchData) {
     console.log("onLaunched: " + JSON.stringify(launchData));
     chrome.identity.getAuthToken({interactive: false}, function(token) {
         console.log("token: " + token);
-        chrome.app.window.create("index.html", null, function(createdWindow) {
-            createdWindow.contentWindow.token = token;
-        });
+        if (token != undefined) {
+            chrome.app.window.create("index.html", null,
+                function(createdWindow) {
+                    createdWindow.contentWindow.token = token;
+                });
+        }
     });
 }
 
